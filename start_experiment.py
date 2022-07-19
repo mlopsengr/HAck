cf6d9f01865440db1c9edab20ee2afac7cb225dc
Tobi John <66161461+tobsiee@users.noreply.github.com>
2022-07-20 00:26:51 +0100

    adds azure experiment file to retrain model

---
+++ start_experiment.py
@@ -0,0 +1,62 @@
+from azureml.core import (
+    Workspace,
+    Experiment,
+    Environment,
+    RunConfiguration,
+    ScriptRunConfig,
+)
+
+
+#Defines compute instance
+compute_name = 'hackcompute22'
+
+#Defines the name of experiment and environment
+experiment_name = 'my-experiment'
+environment_name = 'my-environment'
+
+#Defines name of model and where it is saved
+model_name = 'my-model'
+model_path = 'outputs/model.pkl'
+
+#Defines  directory to run the experiment from
+source_directory = '.'
+
+#Defines the entry script for the experiment
+script_path = 'Hack Partners/traffic.py'
+
+#Defines the location of the machine learning workspace
+subscription_id = '4f67948b-2ff9-49ee-bf1f-90c32dc7545e'
+resource_group  = 'cloud-shell-storage-westeurope'
+workspace_name  = 'Hack_Workspace'
+
+#Connect to workspace
+ws = Workspace(subscription_id = subscription_id, resource_group = resource_group, workspace_name = workspace_name)
+
+#Uses workspace to create an experiment
+exp = Experiment(workspace=ws, name=experiment_name)
+
+#Creates environment with the packages needed
+env = Environment(name=environment_name)
+
+for pip_package in ['numpy','pandas','install torch==1.8.1+cpu torchvision==0.9.1+cpu torchaudio==0.8.1 -f https://download.pytorch.org/whl/torch_stable.html', 'torch torchvision torchaudio', 'matplotlib']:
+    env.python.conda_dependencies.add_pip_package(pip_package)
+
+#Creates a run configuration to connect environment and compute
+run_config = RunConfiguration()
+run_config.target = compute_name
+run_config.environment = env
+
+
+#Creates a script run config to tie all the elements together
+config = ScriptRunConfig(
+    source_directory=source_directory, script=script_path, run_config=run_config
+)
+
+# submitting the experiment to start it
+run = exp.submit(config)
+
+#waits for completion and shows output of the experiment
+run.wait_for_completion(show_output=True, wait_post_processing=True)
+
+#Registers the model with the experiment
+run.register_model(model_name=model_name, model_path=model_path)
